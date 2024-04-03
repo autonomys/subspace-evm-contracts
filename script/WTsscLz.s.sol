8845330a50c194dc548381bf7907d7b13cc516dc
Abhijit Roy <dalvath3700@gmail.com>
2024-04-03 13:39:58 +0530

    Add LZ wrapped TSSC tokens, EndpointV2 with their deploy scripts
    - Change solidity version from 0.8.13 to 0.8.22
    - Add EndpointV2, WethLz contracts with their deploy scripts

---
+++ script/WTsscLz.s.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {WTsscLz} from "../src/WTsscLz.sol";
+
+/* 
+    $ source .env
+    $ forge script script/WTsscLz.s.sol:WTsscLzScript --r $NOVA_RPC_URL --private-key $DEPLOYER_PRIVATE_KEY --broadcast --verify --verifier blockscout --verifier-url $VERIFIER_URL
+
+    // set peers for Sepolia contract
+    $ cast send $MY_LZ_TOKEN_SEPOLIA "setPeer(uint32,bytes32)" $MUMBAI_ENDPOINT_V2_ID $(cast --to-bytes32 $MY_LZ_TOKEN_MUMBAI) --private-key $DEPLOYER_PRIVATE_KEY -r $SEPOLIA_RPC_URL
+    // set peers for Mumbai contract
+    $ cast send $MY_LZ_TOKEN_MUMBAI "setPeer(uint32,bytes32)" $SEPOLIA_ENDPOINT_V2_ID $(cast --to-bytes32 $MY_LZ_TOKEN_SEPOLIA) --private-key $DEPLOYER_PRIVATE_KEY -r $MUMBAI_RPC_URL
+
+    // check if peer correctly set for Sepolia contract
+    $ cast call $MY_LZ_TOKEN_SEPOLIA "isPeer(uint32,bytes32)" $MUMBAI_ENDPOINT_V2_ID $(cast --to-bytes32 $MY_LZ_TOKEN_MUMBAI) -r $SEPOLIA_RPC_URL
+    // check if peer correctly set for Mumbai contract
+    $ cast call $MY_LZ_TOKEN_MUMBAI "isPeer(uint32,bytes32)" $SEPOLIA_ENDPOINT_V2_ID $(cast --to-bytes32 $MY_LZ_TOKEN_SEPOLIA) -r $MUMBAI_RPC_URL
+
+    // get quote of send tokens for Sepolia contract
+    TODO: Add command
+*/
+contract WTsscLzScript is Script {
+    // Provide Endpoint networks where this contract is to be deployed
+    address private epContract = vm.envAddress("NOVA_ENDPOINT_V2");
+    // address private epContract = vm.envAddress("SEPOLIA_ENDPOINT_V2");
+    // address private epContract = vm.envAddress("MUMBAI_ENDPOINT_V2");
+
+    address private delegate;
+
+    function setUp() public {
+        uint256 privateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        delegate = vm.addr(privateKey);
+    }
+
+    function run() public {
+        vm.startBroadcast();
+
+        WTsscLz wTsscLz = new WTsscLz("Wrapped Subspace Token", "WTSSC", epContract, delegate);
+        // Verify the contract was deployed successfully
+        require(address(wTsscLz) != address(0), "Deployment failed");
+        console2.log("WTsscLz contract deployed at ", address(wTsscLz));
+
+        vm.stopBroadcast();
+    }
+}
--- src/Load.sol
+++ src/Load.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.24;
 
 /**
  * This contract is about load testing which means we need to see if high
